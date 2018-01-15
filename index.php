449a28dca3060f6e65985bb4c5482acd03304c39
Daniel Cárcamo <reikchozo@gmail.com>
2018-01-15 02:57:36 -0500

    Maquetando esquema general

--- index.php
+++ index.php
@@ -3,10 +3,251 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Cafe entre montañas</title>
+	<link rel="stylesheet" href="css/ed-grid.min.css">
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/tools.css">
 </head>
 <body>
-	<div class="main-menu">
+	<div class="ed-container full">
+		<nav class="main-menu ed-item s-100">
+			<ul>
+				<li>HOME</li>
+				<li>QUIENES SOMOS</li>
+				<li>PLANES</li>
+				<li>ATRACCIONES</li>
+				<li>BLOG</li>
+				<li>CONTACTO</li>
+			</ul>
+		</nav>
+	</div>
+	<div class="wwa-section ed-container">
+		<div class="ed-item s-100">
+			<h2 class="cvp text-green-title text-center">QUIENES SOMOS</h2>
+		</div>
+		<div class="ed-item s-100 l-30">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod dolor, quas eveniet voluptatem quos commodi nisi! Voluptate ea aliquid iure dolore, rem cupiditate alias, repellat nam officia quidem autem doloremque! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur quo corporis quae nulla blanditiis quod facilis velit at. Odio ut, doloremque, explicabo totam consequuntur temporibus debitis. Autem, cupiditate! Deleniti, impedit.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum autem voluptates eos, tempore et porro impedit ducimus sapiente iusto, itaque. Molestias eveniet nostrum necessitatibus, soluta doloremque porro quisquam ea delectus? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, sunt, laudantium! Earum ipsum dolorem iusto. Amet perspiciatis quisquam sint facilis blanditiis animi, atque sed necessitatibus saepe doloremque suscipit, asperiores, ratione.</p>
+		</div>
+		<div class="ed-item base-100 l-70">
+			<img src="https://dummyimage.com/600x400/000/fff" alt="">
+		</div>
+	</div>
+	<div class="plans-section ed-container flex-h-center full">
+		<div class="ed-item s-100 l-30">
+			<div class="plans-section__card backg-white">
+				<div class="plans-section__card-title backg-cyan">
+					<h3 class="text-center cvp"><span class="text-1rem">PLAN</span><br> ALMENDRA</h3>
+				</div>
+				<div class="plans-section__card-body">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dolorum quidem, saepe molestiae dicta. Delectus explicabo excepturi id maiores. Mollitia delectus, at recusandae nesciunt dolore voluptatem et quam aperiam excepturi?</p>	
+				</div>
+				<div class="plans-section__card-footer text-center">
+					<a href="#" class="text-white backg-green">ELEGIR PLAN</a>
+				</div>
+			</div>
+		</div>
+		<div class="ed-item s-100 l-30">
+			<div class="plans-section__card backg-white">
+				<div class="plans-section__card-title backg-orange">
+					<h3 class="text-center cvp"><span class="text-1rem">PLAN</span><br>REGIONAL</h3>
+				</div>
+				<div class="plans-section__card-body">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dolorum quidem, saepe molestiae dicta. Delectus explicabo excepturi id maiores. Mollitia delectus, at recusandae nesciunt dolore voluptatem et quam aperiam excepturi?</p>	
+				</div>
+				<div class="plans-section__card-footer text-center">
+					<a href="#" class="text-white backg-green">ELEGIR PLAN</a>
+				</div>
+			</div>
+		</div>
+		<div class="ed-item s-100 l-30">
+			<div class="plans-section__card backg-white">
+				<div class="plans-section__card-title backg-yellow">
+					<h3 class="text-center cvp"><span class="text-1rem">PLAN</span><br>COSECHA</h3>
+				</div>
+				<div class="plans-section__card-body">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dolorum quidem, saepe molestiae dicta. Delectus explicabo excepturi id maiores. Mollitia delectus, at recusandae nesciunt dolore voluptatem et quam aperiam excepturi?</p>	
+				</div>
+				<div class="plans-section__card-footer text-center">
+					<a href="#" class="text-white backg-green">ELEGIR PLAN</a>
+				</div>
+			</div>
+		</div>
+		<div class="ed-item s-100">
+			<p class="text-center plans-section__observation">Todos nuestros planes y alojamientos son con reserva previa. Comunicate con nosotros, es un placer atenderte</p>
+		</div>
+	</div>
+	<div class="invitation-section ed-container full flex-h-center backg-green">
+		<div class="ed-item s-100 l-40">
+			<p class="text-center text-white">Vive una experiencia única con olor a cafe entre montañas</p>	
+		</div>
+	</div>
+	<div class="attractions-section ed-container flex-h-center">
+		<div class="ed-item s-100">
+			<h2 class="cvp text-green-title text-center">ATRACCIONES</h2>
+		</div>
+		<div class="ed-item s-100 l-20 attractions-section__box backg-cyan">
+			<div class="attractions-section__box-text">
+				<h4>Lorem Impsum dolor</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+			</div>
+		</div>
+		<div class="ed-item s-100 l-20 attractions-section__box backg-cyan">
+			<div class="attractions-section__box-text">
+				<h4>Lorem Impsum dolor</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+			</div>
+		</div>
+		<div class="ed-item s-100 l-20 attractions-section__box backg-cyan">
+			<div class="attractions-section__box-text">
+				<h4>Lorem Impsum dolor</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+			</div>
+		</div>
+		<div class="ed-item s-100 l-20 attractions-section__box backg-cyan">
+			<div class="attractions-section__box-text">
+				<h4>Lorem Impsum dolor</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+			</div>
+		</div>
+		<div class="ed-item s-100 l-20 attractions-section__box backg-cyan">
+			<div class="attractions-section__box-text">
+				<h4>Lorem Impsum dolor</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+			</div>
+		</div>
+		<div class="ed-item s-100 l-20 attractions-section__box backg-cyan">
+			<div class="attractions-section__box-text">
+				<h4>Lorem Impsum dolor</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+			</div>
+		</div>
+		<div class="ed-item s-100 l-20 attractions-section__box backg-cyan">
+			<div class="attractions-section__box-text">
+				<h4>Lorem Impsum dolor</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+			</div>
+		</div>
+		<div class="ed-item s-100 l-20 attractions-section__box backg-cyan">
+			<div class="attractions-section__box-text">
+				<h4>Lorem Impsum dolor</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+			</div>
+		</div>
+	</div>
+	<div class="backg-gray">
+		<div class="blogs-section ed-container flex-h-center">
+			<div class="ed-item s-100">
+				<h2 class="cvp text-green-title text-center">BLOG</h2>
+			</div>
+			<div class="ed-item s-100 l-50">
+				<div class="blogs-section__card">
+					<img src="https://dummyimage.com/600x200/000/fff" alt="">
+					<div class="blogs-section__card-text">
+						<p class="text-green-title">LOREM IMPSUMP DOLOR</p>
+					</div>
+				</div>
+			</div>
+			<div class="ed-item s-100 l-50">
+				<div class="blogs-section__card">
+					<img src="https://dummyimage.com/600x200/000/fff" alt="">
+					<div class="blogs-section__card-text">
+						<p class="text-green-title">LOREM IMPSUMP DOLOR</p>
+					</div>
+				</div>
+			</div>
+			<div class="ed-item s-100 l-25">
+				<div class="blogs-section__card">
+					<img src="https://dummyimage.com/600x200/000/fff" alt="">
+					<div class="blogs-section__card-text">
+						<p class="text-green-title">LOREM IMPSUMP DOLOR</p>
+					</div>
+				</div>
+			</div>
+			<div class="ed-item s-100 l-25">
+				<div class="blogs-section__card">
+					<img src="https://dummyimage.com/600x200/000/fff" alt="">
+					<div class="blogs-section__card-text">
+						<p class="text-green-title">LOREM IMPSUMP DOLOR</p>
+					</div>
+				</div>
+			</div>
+			<div class="ed-item s-100 l-25">
+				<div class="blogs-section__card">
+					<img src="https://dummyimage.com/600x200/000/fff" alt="">
+					<div class="blogs-section__card-text">
+						<p class="text-green-title">LOREM IMPSUMP DOLOR</p>
+					</div>
+				</div>
+			</div>
+			<div class="ed-item s-100 l-25">
+				<div class="blogs-section__card">	
+					<img src="https://dummyimage.com/600x200/000/fff" alt="">
+					<div class="blogs-section__card-text">
+						<p class="text-green-title">LOREM IMPSUMP DOLOR</p>
+					</div>
+				</div>
+			</div>
+			<div class="ed-item s-100 l-25">
+				<div class="blogs-section__card">
+					<img src="https://dummyimage.com/600x200/000/fff" alt="">
+					<div class="blogs-section__card-text">
+						<p class="text-green-title">LOREM IMPSUMP DOLOR</p>
+					</div>
+				</div>
+			</div>
+			<div class="ed-item s-100 l-25">
+				<div class="blogs-section__card">
+					<img src="https://dummyimage.com/600x200/000/fff" alt="">
+					<div class="blogs-section__card-text">
+						<p class="text-green-title">LOREM IMPSUMP DOLOR</p>
+					</div>
+				</div>
+			</div>
+			<div class="ed-item s-100 l-25">
+				<div class="blogs-section__card">		
+					<img src="https://dummyimage.com/600x200/000/fff" alt="">
+					<div class="blogs-section__card-text">
+						<p class="text-green-title">LOREM IMPSUMP DOLOR</p>
+					</div>
+				</div>
+			</div>
+			<div class="ed-item s-100 l-25">
+				<div class="blogs-section__card">	
+					<img src="https://dummyimage.com/600x200/000/fff" alt="">
+					<div class="blogs-section__card-text">
+						<p class="text-green-title">LOREM IMPSUMP DOLOR</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="moral-section ed-container flex-h-center">
+		<div class="ed-item s-100 l-20">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolore atque doloremque, saepe dolor consequatur molestias nihil optio? Placeat voluptate hic minima impedit deserunt ducimus optio excepturi, numquam voluptatem iusto.
+		</div>
+		<div class="ed-item s-100 l-20">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non impedit quo atque dolorum doloribus, porro minima libero consectetur, alias perspiciatis maxime incidunt magni voluptate magnam et culpa quidem illum iusto.
+		</div>
+		<div class="ed-item s-100 l-20">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum facilis eius quaerat aliquid impedit, laudantium quae mollitia incidunt libero. Cumque incidunt fugiat voluptates doloribus fuga assumenda, itaque eum ullam maiores!
+		</div>
+		<div class="ed-item s-100 l-20">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus possimus pariatur, quis enim itaque illo vitae optio expedita earum ab libero quod at impedit rerum aliquid labore provident eius! Nihil?
+		</div>
+	</div>
+	<div class="map-section">
 		
 	</div>
+	<footer class="ed-container flex-h-center text-white">
+		<div class="ed-item s-100 l-25">
+			<h4>CONTACTO</h4>
+		</div>
+		<div class="ed-item s-100 l-25">
+			<h4>SIGUENOS</h4>
+		</div>
+		<div class="ed-item s-100 l-25">
+			<h4>ALIADOS</h4>
+		</div>
+	</footer>
 </body>
 </html>
